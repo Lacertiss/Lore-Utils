91cc6d72e39cb29ebc1d71688f494d3db560b52f
lacertis <lacertistestacc@gmail.com>
2025-04-30 22:05:33 +0200

    Add ESPRender & AreaChecker

--- src/main/java/de/lacertis/LoreUtilsClient.java
+++ src/main/java/de/lacertis/LoreUtilsClient.java
@@ -1,10 +1,20 @@
 package de.lacertis;
 
+import de.lacertis.client.AreaChecker;
+import de.lacertis.client.EspRender;
 import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
 
 public class LoreUtilsClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        Box testBox = new Box(0, 70, 0, 10, 100, 10);
 
+        AreaChecker.addArea(testBox);
+        EspRender.registerPosition(new BlockPos(100, 64, 100));
+
+        AreaChecker.init();
+        EspRender.init();
     }
 }
---
+++ src/main/java/de/lacertis/client/AreaChecker.java
@@ -0,0 +1,30 @@
+package de.lacertis.client;
+
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Vec3d;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AreaChecker {
+    private static final List<Box> AREAS = new ArrayList<>();
+
+    public static void addArea(Box area) {
+        AREAS.add(area);
+    }
+
+    public static void init() {
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (client.world != null && client.player != null) {
+                Vec3d playerPos = client.player.getPos();
+                for (Box area : AREAS) {
+                    if (area.contains(playerPos)) {
+                        System.out.println("Spieler ist in einer bestimmten Area!");
+                    }
+                }
+            }
+        });
+    }
+}
