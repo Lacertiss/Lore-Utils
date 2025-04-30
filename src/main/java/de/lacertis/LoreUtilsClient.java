60904449836f6ef3bf191f591d4302640168bffe
lacertis <lacertistestacc@gmail.com>
2025-04-30 20:25:07 +0200

    Initial commit

---
+++ src/main/java/de/lacertis/LoreUtilsClient.java
@@ -0,0 +1,10 @@
+package de.lacertis;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LoreUtilsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
